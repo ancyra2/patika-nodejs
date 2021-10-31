ba42933836a446b3807ad6e312456df601b5862b
ancyra2 <duran.talih@gmail.com>
2021-10-31 22:11:06 +0300

    Node.js Ödev 1

--- odev1/odev1.js
+++ odev1/odev1.js
@@ -3,8 +3,7 @@ let pi = Math.PI;
 
 function circleArea(r){
   area = pi * r * r;
-  console.log(r);
   console.log(`Yarı çapı ${r} olan darinenin alanı : ${area}'dir.`);
 }
-console.log(args);
+
 circleArea(args[0] * 1);
