962a4157f14c0266ceecd4fc91e94372081422bd
ancyra2 <duran.talih@gmail.com>
2021-11-03 10:17:38 +0300

    Node.js Ödev 5

---
+++ odev-5/server.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  console.log("Selam");
+  switch (url) {
+    case "/":
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.write("<h1>Index Sayfasi</h1>");
+      break;
+    case "/about":
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.write("<h1>About Sayfasi</h1>");
+      break;
+    case "/contact":
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.write("<h1>Contact Sayfasi</h1>");
+      break;
+    default:
+      res.writeHead(404, {"Content-Type" : "text/html"});
+      res.write("<h1>Aranan Sayfa Bulunamadi</h1>");
+  }
+  res.end();
+});
+
+server.listen(port, ()=>{
+  console.log(`Sunucuya ${port} undan başarıyla bağlanıldı.`);
+})
